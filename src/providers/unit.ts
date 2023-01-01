8dd8f7a7af0417f0acfce6ddc83511d08cc43dcf
Stanislav Romanov <kaermorchen@gmail.com>
2023-01-01 11:01:02 +0300

    Add spawn request

--- src/providers/unit.ts
+++ src/providers/unit.ts
@@ -1,3 +1,4 @@
+import { spawn } from 'child_process';
 import {
   Uri,
   EventEmitter,
@@ -9,6 +10,8 @@ import {
 
 export default class UnitProvider implements TextDocumentContentProvider {
   static scheme = 'nginx-unit';
+
+  config: unknown;
 
   #onDidChange = new EventEmitter<Uri>();
   // #documents = new Map<string, ReferencesDocument>();
@@ -34,7 +37,52 @@ export default class UnitProvider implements TextDocumentContentProvider {
     return this.#onDidChange.event;
   }
 
-  provideTextDocumentContent(uri: Uri): string {
-    return 'Hello' + uri.path;
+  async provideTextDocumentContent(uri: Uri): Promise<string> {
+    try {
+      await this.connectToUnit();
+    } catch (error) {
+      console.error(error);
+    }
+
+    const text = JSON.stringify(this.config, null, 2);
+
+    return text;
+  }
+
+  async connectToUnit() {
+    const spawnOptions = {
+      encoding: 'utf8',
+      timeout: 1000 * 60 * 1, // 1 minute
+    };
+    const args = [
+      '--silent',
+      '--unix-socket',
+      '/var/run/control.unit.sock',
+      'http://localhost/config/',
+    ];
+
+    return new Promise((resolve, reject) => {
+      // TODO: add abort signal
+      const curl = spawn('curl', args, spawnOptions);
+
+      curl.stdout.on('data', (data) => {
+        const config = JSON.parse(data);
+
+        this.config = config;
+        resolve(0);
+      });
+
+      curl.stderr.on('data', (x) => {
+        reject(x.toString());
+      });
+
+      curl.on('exit', (code) => {
+        if (code === 0) {
+          resolve(code);
+        } else {
+          reject(code);
+        }
+      });
+    });
   }
 }
